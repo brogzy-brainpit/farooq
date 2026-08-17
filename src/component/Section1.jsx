71ecda0086e5f9e84964deb1addb4ca76951b144
unknown <dangabarin2020@gmail.com>
2026-08-17 13:58:41 +0100

    update commit

--- src/component/Section1.jsx
+++ src/component/Section1.jsx
@@ -16,10 +16,10 @@ function Section1() {
               <Description/>
             </div>
 
-            <div ref={track} className="col-span-5 col-start-2 lg:col-start-1 lg:col-span-5 aspect-[7/5]">
+            <div ref={track} className=" lg:order-1 col-span-5 col-start-2 lg:col-start-1 lg:col-span-5 aspect-[7/5]">
               <ParallaxImage track={track} src={"/assets/yellow-car.png"}/>
             </div>
-             <div className="col-start-2 col-span-4 lg:col-start-7 lg:col-span-6 ">
+             <div className="order-3 lg:order-2 col-start-2 col-span-4 lg:col-start-7 lg:col-span-6 ">
                 <TypographyReveal className='font-custom text-brand-white text-heading3 capitalie leading-[1.1]'
     toColor='#f6efe4'
       animationType='fadeInUp'
@@ -27,6 +27,9 @@ function Section1() {
         revealType='word'  >
 Expert Car Detaiing: from luxury brands to your everyday ride in Miami
       </TypographyReveal>
+            </div>
+             <div ref={track} className="order-2 lg:order-3 col-span-3 col-start-1 lg:col-start-10 lg:col-span-3 aspect-[7/5]">
+              <ParallaxImage track={track} src={"/assets/red-benz.png"}/>
             </div>
           </GridColumn>
         </Section>
